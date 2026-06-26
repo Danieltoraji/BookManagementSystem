a823613abb964ec981818d7ce0199eb616f6f139
钉小呆Xiaodai. <98793646+Danieltoraji@users.noreply.github.com>
2026-06-27 00:14:51 +0800

    新增图书搜索功能，支持按ISBN、标题和作者进行多条件查询

--- services/bookservice.cpp
+++ services/bookservice.cpp
@@ -64,17 +64,157 @@ bool BookService::updateBook(const std::string& isbn, const Book& book)
 	return false;
 }
 
-
-
-/*
-std::vector<const Book*> BookService::searchBooksByISBN(const std::string& isbn) const
+std::vector<const Book*> BookService::searchBooks(const searchQuery& query) const
 {
-	std::vector<const Book*> result;
-	for (const auto& book : books) {
-		if (book.getISBN() == isbn) {
-			result.push_back(&book);
-		}
-	}
-	return result;
+    std::vector<const Book*> result;
+    std::vector<bool> flag1, flag2;
+    switch(query.field1){
+        case searchQuery::Field::ISBN:
+            switch(query.condition1){
+                case searchQuery::Condition::Equals:
+                    for (const auto& book : books){
+                        if(book.getISBN()==query.value1) flag1.push_back(true);
+                        else flag1.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::Contains:
+                    for (const auto& book : books){
+                        if(book.getISBN().find(query.value1)!=std::string::npos) flag1.push_back(true);
+                        else flag1.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::StartsWith:
+                    for (const auto& book : books){
+                        if(book.getISBN().rfind(query.value1, 0) == 0) flag1.push_back(true);//最后一次出现是在开头
+                        else flag1.push_back(false);
+                    }
+                break;
+            }
+        break;
+        case searchQuery::Field::Title:
+            switch(query.condition1){
+                case searchQuery::Condition::Equals:
+                    for (const auto& book : books){
+                        if(book.getTitle()==query.value1) flag1.push_back(true);
+                        else flag1.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::Contains:
+                    for (const auto& book : books){
+                        if(book.getTitle().find(query.value1)!=std::string::npos) flag1.push_back(true);
+                        else flag1.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::StartsWith:
+                    for (const auto& book : books){
+                        if(book.getTitle().rfind(query.value1, 0) == 0) flag1.push_back(true);
+                        else flag1.push_back(false);
+                    }
+                break;
+            }break;
+        case searchQuery::Field::Author:
+            switch(query.condition1){
+                case searchQuery::Condition::Equals:
+                    for (const auto& book : books){
+                        if(book.getAuthor()==query.value1) flag1.push_back(true);
+                        else flag1.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::Contains:
+                    for (const auto& book : books){
+                        if(book.getAuthor().find(query.value1)!=std::string::npos) flag1.push_back(true);
+                        else flag1.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::StartsWith:
+                    for (const auto& book : books){
+                        if(book.getAuthor().rfind(query.value1, 0) == 0) flag1.push_back(true);
+                        else flag1.push_back(false);
+                    }
+                    break;
+            }
+        break;
+    }
+            
+    switch(query.field2){
+        case searchQuery::Field::ISBN:
+            switch(query.condition2){
+                case searchQuery::Condition::Equals:
+                    for (const auto& book : books){
+                        if(book.getISBN()==query.value2) flag2.push_back(true);
+                        else flag2.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::Contains:
+                    for (const auto& book : books){
+                        if(book.getISBN().find(query.value2)!=std::string::npos) flag2.push_back(true);
+                        else flag2.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::StartsWith:
+                    for (const auto& book : books){
+                        if(book.getISBN().rfind(query.value2, 0) == 0) flag2.push_back(true);//最后一次出现是在开头
+                        else flag2.push_back(false);
+                    }
+                break;
+            }
+        break;
+        case searchQuery::Field::Title:
+            switch(query.condition2){
+                case searchQuery::Condition::Equals:
+                    for (const auto& book : books){
+                        if(book.getTitle()==query.value2) flag2.push_back(true);
+                        else flag2.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::Contains:
+                    for (const auto& book : books){
+                        if(book.getTitle().find(query.value2)!=std::string::npos) flag2.push_back(true);
+                        else flag2.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::StartsWith:
+                    for (const auto& book : books){
+                        if(book.getTitle().rfind(query.value2, 0) == 0) flag2.push_back(true);
+                        else flag2.push_back(false);
+                    }
+                break;
+            }break;
+        case searchQuery::Field::Author:
+            switch(query.condition2){
+                case searchQuery::Condition::Equals:
+                    for (const auto& book : books){
+                        if(book.getAuthor()==query.value2) flag2.push_back(true);
+                        else flag2.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::Contains:
+                    for (const auto& book : books){
+                        if(book.getAuthor().find(query.value2)!=std::string::npos) flag2.push_back(true);
+                        else flag2.push_back(false);
+                    }
+                break;
+                case searchQuery::Condition::StartsWith:
+                    for (const auto& book : books){
+                        if(book.getAuthor().rfind(query.value2, 0) == 0) flag2.push_back(true);
+                        else flag2.push_back(false);
+                    }
+                    break;
+            }
+        break;
+
 }
-*/
+
+    switch(query.logic){
+        case searchQuery::Logic::And:
+            for(size_t i=0;i<books.size();i++){
+                if(flag1[i] && flag2[i]) result.push_back(&books[i]);
+            }
+        break;
+        case searchQuery::Logic::Or:
+            for(size_t i=0;i<books.size();i++){
+                if(flag1[i] || flag2[i]) result.push_back(&books[i]);
+            }
+        break;
+    }
+}
