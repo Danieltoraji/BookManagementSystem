c8aaf351ba896bde1a12a46dd00bd8856efe53bc
钉小呆Xiaodai. <98793646+Danieltoraji@users.noreply.github.com>
2026-06-30 20:10:34 +0800

    bugfix

--- model/loan.cpp
+++ model/loan.cpp
@@ -1,9 +1,9 @@
 #include "loan.h"
 
 Loan::Loan(std::string id, std::string libCode, std::string ISBN, std::string userId,
-           Date loanDate, Date dueDate, Date returnDate):
+           Date loanDate, Date dueDate, Date returnDate, bool isReturned):
     id(id), libCode(libCode), ISBN(ISBN), userId(userId),
-    loanDate(loanDate), dueDate(dueDate), returnDate(returnDate)
+    loanDate(loanDate), dueDate(dueDate), returnDate(returnDate), isReturned(isReturned)
 {
 }
 
@@ -11,14 +11,18 @@ Loan::Loan(std::string id, std::string libCode, std::string ISBN, std::string us
 std::string Loan::getId() const { return id; }
 std::string Loan::getLibCode() const { return libCode; }
 std::string Loan::getUserId() const { return userId; }
+std::string Loan::getISBN() const { return ISBN; }
 Date Loan::getLoanDate() const { return loanDate; }
 Date Loan::getDueDate() const { return dueDate; }
 Date Loan::getReturnDate() const { return returnDate; }
+bool Loan::getIsReturned() const { return isReturned; }
 
 // Setters
 void Loan::setId(const std::string &value) { id = value; }
 void Loan::setLibCode(const std::string &value) { libCode = value; }
 void Loan::setUserId(const std::string &value) { userId = value; }
+void Loan::setISBN(const std::string &value) { ISBN = value; }
 void Loan::setLoanDate(const Date &value) { loanDate = value; }
 void Loan::setDueDate(const Date &value) { dueDate = value; }
 void Loan::setReturnDate(const Date &value) { returnDate = value; }
+void Loan::setIsReturned(bool value) { isReturned = value; }
